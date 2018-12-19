8b819fa0cedaa97eadf5065797b8e7a34983ceca
https://orahub.oraclecorp.com/rajdeep_bhattacharya <rajdeep.bhattacharya@oracle.com>
2018-12-19 08:32:13 +0530

    added dotenv made changes to slackclient.js

--- server/slackCient.js
+++ server/slackCient.js
@@ -1,14 +1,14 @@
 const { RTMClient } = require('@slack/client');
 
 // An access token (from your Slack app or custom integration - usually xoxb)
-const token = 'xoxb-506357030896-506942755380-z984IjM7yrflMTdNTP5xf4Yr';
+const token = process.env.bot_user_auth_access_token;
 
 // The client is initialized and then started to get an active connection to the platform
 const rtm = new RTMClient(token);
 rtm.start();
 
 // This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
-const conversationId = 'C1232456';
+const conversationId = 'CEWT7BLFN';
 
 // The RTM client can send simple string messages
 rtm.sendMessage('Hello there', conversationId)
